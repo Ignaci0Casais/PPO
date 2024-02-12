f69466a1bf2a9b810ceaf027c2bf7003f9f0b495
Ignacio Casais <135152304+Ignaci0Casais@users.noreply.github.com>
2024-02-12 19:05:27 -0300

    Add files via upload

---
+++ Final.Aplicacion/CalculoResta.cs
@@ -0,0 +1,12 @@
+﻿using Final.Core;
+
+namespace Final.Aplicacion
+{
+    public class CalculoResta : ICalculo
+    {
+        public int Calcular(Datos datos)
+        {
+            return datos.Numero1 - datos.Numero2;
+        }
+    }
+}
